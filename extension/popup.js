037f2290e7ef5e4c3e9a0e225e41147a48d16abd
Matt Fredrikson <mattfredrikson@gmail.com>
2012-04-12 22:51:23 -0500

    Added Ben's initial front-end code

--- extension/popup.js
+++ extension/popup.js
@@ -1,3 +1,10 @@
+$(function() {
+    $('div.tabs').tabs();
+    var imgURL = chrome.extension.getURL('images/bucket.jpg');
+    //document.getElementById('bucket-img').src = imgURL;
+    //console.log(imgURL);
+    $('#tabs-right').css("background-image" : imgURL);
+});
 
 // will contain code that examines that local store
 // containing tracker/host page links, computes probabilities,
