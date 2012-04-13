a8d0a0a1c47738e4541fe9936c37c9d079940e3a
Matt Fredrikson <mattfredrikson@gmail.com>
2012-04-13 14:00:30 -0500

    Added a few odds and ends. Will comment more later.

---
+++ backend/phantomscript.js
@@ -0,0 +1,53 @@
+
+// expects:
+// first argument to be the host whose demographics we scrape from quantcast
+// second argment to be the output file to append to
+
+var fs = require('fs');
+var system = require('system');
+
+var host = system.args[1];
+var outfile = system.args[2];
+fs.touch(outfile);
+var fp = fs.open(outfile,"a");
+
+// format: hostdomain,demographics vector
+fp.write(host + ",");
+
+var page = require('webpage').create();
+console.log('The default user agent is ' + page.settings.userAgent);
+console.log('Opening ' + 'http://www.quantcast.com/' + host + '/demographics');
+
+// open up the quantcast page to scrape
+page.open('http://www.quantcast.com/' + host + '/demographics',
+    function (status) {
+        if(status !== 'success')
+            console.log('Unable to open quantcast.com');
+        else {
+            
+            // Scrape the age results
+            var ethresults = page.evaluate(function () {
+                var body = document.body;
+                var list = body.querySelectorAll('div#panel-AGE * table[class=demographics-composition] * td[class~=index-digit]');
+                var ret = [];
+                for(var i = 0; i < list.length; i++)
+                    ret.push(list[i].innerText);
+                return ret;
+            });
+            
+            // write the age results to the output file
+            for(var i = 0; i < ethresults.length; i++) {
+                if(ethresults[i] == null) continue;
+                
+                fp.write(ethresults[i].replace('%',''));
+                if(i < ethresults.length-1) fp.write(',');
+                console.log(ethresults[i].replace('%',''));
+            }
+            
+            // signal end of host
+            fp.write('\n');
+        }
+        fp.close();
+        phantom.exit();
+    }
+);
--- extension/background.js
+++ extension/background.js
@@ -3,6 +3,33 @@ Object.prototype.hasOwnProperty = function(property) {
     return typeof(this[property]) !== 'undefined'
 };
 
+// quick and dirty url parser
+// this doesn't actually work for all domains - we will have to fix it
+function parseURL(url) {
+    var a =  document.createElement('a');
+    a.href = url;
+    var domsplits = a.hostname.split(".");
+    var dom = domsplits[domsplits.length-2]+"."+domsplits[domsplits.length-1];
+    return ret = {
+        source: url,
+        protocol: a.protocol.replace(':',''),
+        hostdomain: dom
+    };
+}
+
+// gets a tracker network name from a domain name
+// returns the input domain name if it is not found in our list
+function getNetworkFromDomain(domain) {
+    
+    for(var el in ad_networks) {
+        
+        if(el.domain == domain)
+            return el.name;
+    }
+    
+    return domain;
+}
+
 // sends an update to the back-end demographic server,
 // notifying it of a new host page/tracker observation.
 // the demographics server should send us demographics
@@ -32,14 +59,43 @@ function updateDemographicServer(userid, req) {
             }
         }
         
-        // TODO: map third-party domains to tracker names
-        
         // we're an extension, so this request can go anywhere
-        xhr.open("GET", "https://demo-server/update.php?domain=" + req.hostpage + "&" + req.thirdparties[tp], true);
+        xhr.open("GET", "https://demo-server/update.php?domain=" +
+                 req.hostpage +
+                 "&" +
+                 getNetworkFromDomain(req.thirdparties[tp]),
+                 true);
         xhr.send();
     }
 }
 
+// this is run once when the extension is installed.
+// scans the user's history, and establishes host page/tracker
+// links from the fourthparty data.
+function seedDbFromHistory() {
+    
+    // i wonder if there is a way to specify an unlimited number of results...
+    var hist = chrome.history.search({"maxResults": 10000},
+        function (hist_items) {
+            
+            for(var item in hist_items) {
+                                
+                var url = hist_items[item].url;
+                var hostdomain = parseURL(url).hostdomain;
+                
+                var cur_trackers = trackers[hostdomain];
+                
+                for(var tracker in cur_trackers) {
+                    
+                    insertIntoDb({"hostpage": hostdomain,
+                                  "thirdparties": [getNetworkFromDomain(cur_trackers[tracker])]},
+                                 hist_item[item].visit_count);
+                }
+                
+            }
+        }
+    );
+}
 
 // insertIntoDb updates the local store with a new "link"
 // between a host page and a tracker. a count of the number
@@ -49,19 +105,21 @@ function updateDemographicServer(userid, req) {
 // req should have two members
 // hostpage: the domain of the page hosting the trackers
 // thirdparties: a list of tracker domains on hostpage
-function insertIntoDb(req) {
+//
+// count (optional) is the number of times the connection was observed
+// if count is undefined, then the link counter is incremented
+// otherwise it is set to count
+function insertIntoDb(req, count) {
     
     for(var tp in req.thirdparties) {
         
-        // TODO: map third-party domains to tracker names
-        
-        var key = req.hostpage + ';' + req.thirdparties[tp];
+        var key = req.hostpage + ';' + getNetworkFromDomain(req.thirdparties[tp]);
         var cur = localStorage[key];
             
         if(cur == undefined)
-            localStorage[key] = 1;
+            localStorage[key] = (count == undefined) ? 1 : count;
         else
-            localStorage[key] = parseInt(localStorage[key]) + 1;
+            localStorage[key] = (count == undefined) ? parseInt(localStorage[key]) + 1 : count;
     }
 }
 
