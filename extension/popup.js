68e4057afaf6d151ee9f4ce0622c5a2c985e7ae1
nrabinowitz <public@nickrabinowitz.com>
2012-04-14 14:16:04 -0400

    Overview table and bars

--- extension/popup.js
+++ extension/popup.js
@@ -36,9 +36,57 @@ $(function() {
 			} else {
 				$('#tracker-image').hide();
 			}
+			// update report tabs
+			updateOverview(data);
 			$('#view-tabs').tabs("option", "selected", 0);
 			
 		})
 		// run it for the first tracker (all data)
 		.first().click();
+		
 });
+
+function updateOverview(data) {
+	// update support count
+	$('#support-count').html(data.support);
+	// update overview table
+	$('#overview-table tbody').empty();
+	
+	// scales for overview data
+	var width = d3.scale.linear()
+			.domain([0,100])
+			.range([0, 80]),
+		color = d3.scale.linear()
+			.domain([0,100])
+			.range(['red', 'blue']);
+	
+	// add overview rows
+	['age', 'income', 'gender', 'education', 'family', 'ethnicity'].forEach(function(field) {
+	
+		// slightly ugly title case conversion
+		var title = field.replace(/\w\S*/g, function(txt){
+				return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+			}),
+			// get the top guess
+			entries = d3.entries(data[field]);
+		entries.sort(function(a,b) { return d3.descending(a.value, b.value)});
+		var top = entries[0];
+		
+		// add rows
+		$('#overview-table tbody').append(
+			'<tr class="' + field + '"><th>' + title + '</th><td>' + top.key + '</td><td class="confidence">' + top.value + '%</td></tr>'
+		);
+		
+		// color value and add bar
+		var visCell = d3.select('#overview-table tr.' + field + ' td.confidence');
+		visCell.style('color', color(top.value));
+		visCell.append('svg:svg')
+				.attr('height', 12)
+				.attr('width', 100)
+			.append('svg:rect')
+				.attr('x', 10)
+				.attr('height', 15)
+				.attr('width', width(top.value))
+				.attr('fill', color(top.value));
+	})
+}
