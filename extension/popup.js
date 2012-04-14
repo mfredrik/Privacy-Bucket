ad8c0034742114d36e20c538cd641e4ab298cd7e
nrabinowitz <public@nickrabinowitz.com>
2012-04-14 13:26:43 -0400

    tracker images and links added

--- extension/popup.js
+++ extension/popup.js
@@ -7,7 +7,7 @@ $(function() {
 	trackers.forEach(function(name, index) {
 		var id = name.replace(/\W/g, '_'),
 			checked = !index ? 'checked="checked"' : '';
-		$('#tracker-tabs').append('<input type="radio" id="' + id + '" name="tracker" ' + checked + ' /><label for="' + id +'">' + name + '</label>');
+		$('#tracker-tabs').append('<input type="radio" id="' + id + '" name="tracker" ' + checked + '/><label for="' + id +'">' + name + '</label>');
 	});
 	
 	// set up nav functionality
@@ -16,10 +16,29 @@ $(function() {
 		.removeClass('ui-corner-right ui-corner-left')
 		.click(function() {
 			var $label = $(this),
-				id = $label.attr('for'),
-				title =  $label.text();
-				
-			$('#report-pane h2').html(title);
+				key = $label.text(),
+				data = getPerTrackerDemographics(key),
+				networkId = data.network_id > 0 && data.network_id,
+				networkUrl = networkId && ('http://privacychoice.org/companies/index/' + networkId);
+			// set the title and image
+			$('#tracker-title').html(key);
+			if (networkId) {
+				// add more info link
+				$('#tracker-title')
+					.append('<span class="moreinfo">(<a href="' + networkUrl + '" target="_blank">More Info</a>)</span>');
+				// image
+				$('#tracker-image').show();
+				$('#tracker-image a')
+					.attr('href', networkUrl);
+				$('#tracker-image img')
+					.attr('src', 'http://images.privacychoice.org/images/network/' + networkId + '.jpg')
+					.attr('alt', key);
+			} else {
+				$('#tracker-image').hide();
+			}
 			$('#view-tabs').tabs("option", "selected", 0);
-		});
+			
+		})
+		// run it for the first tracker (all data)
+		.first().click();
 });
