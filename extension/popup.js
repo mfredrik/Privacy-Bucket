0887895dae210c8debd211f07a7ad843bad81d70
nrabinowitz <public@nickrabinowitz.com>
2012-04-14 12:30:50 -0400

    trackers created dynamically

--- extension/popup.js
+++ extension/popup.js
@@ -1,16 +1,25 @@
 $(function() {
 	// layout setup
-	$('#tracker-tabs').buttonset();
-	$('#tracker-tabs label').removeClass('ui-corner-right ui-corner-left');
 	$('#view-tabs').tabs();
 	
-	// nav setup
-	$('#tracker-tabs label').click(function() {
-		var $label = $(this),
-			id = $label.attr('for'),
-			title =  $label.text();
-			
-		$('#report-pane h2').html(title);
-		$('#view-tabs').tabs("option", "selected", 0);
-	})
+	// add current trackers to nav
+	var trackers = getAdvertisers();
+	trackers.forEach(function(name, index) {
+		var id = name.replace(/\W/g, '_'),
+			checked = !index ? 'checked="checked"' : '';
+		$('#tracker-tabs').append('<input type="radio" id="' + id + '" name="tracker" ' + checked + ' /><label for="' + id +'">' + name + '</label>');
+	});
+	
+	// set up nav functionality
+	$('#tracker-tabs').buttonset();
+	$('#tracker-tabs label')
+		.removeClass('ui-corner-right ui-corner-left')
+		.click(function() {
+			var $label = $(this),
+				id = $label.attr('for'),
+				title =  $label.text();
+				
+			$('#report-pane h2').html(title);
+			$('#view-tabs').tabs("option", "selected", 0);
+		});
 });
