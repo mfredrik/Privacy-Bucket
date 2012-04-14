852c4a25aa840b44821f5ace7f5ae65f245b7aac
nrabinowitz <public@nickrabinowitz.com>
2012-04-14 11:57:19 -0400

    static UI working

--- extension/popup.js
+++ extension/popup.js
@@ -1,6 +1,18 @@
 $(function() {
+	// layout setup
 	$('#tracker-tabs').buttonset();
 	$('#tracker-tabs label').removeClass('ui-corner-right ui-corner-left');
+	$('#view-tabs').tabs();
+	
+	// nav setup
+	$('#tracker-tabs label').click(function() {
+		var $label = $(this),
+			id = $label.attr('for'),
+			title =  $label.text();
+			
+		$('#report-pane h2').html(title);
+		console.log(id);
+	})
 });
 
 tabCounter = 0;
